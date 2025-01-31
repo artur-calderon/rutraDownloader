355db6c3544836f13016736732194ed7937983dc
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2025-01-31 13:43:54 -0400

    cookies from browser

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def download_video(url, progress_hook=None):
         'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
         'format': 'best',
         'progress_hooks': [progress_hook] if progress_hook else [],
+        'cookies':'--cookies-from-browser chrome',
         'ffmpeg_location': 'C:/Program Files/ffmpeg/bin',
         'ffprobe_location': 'C:/Program Files/ffmpeg/bin',
     }
@@ -44,6 +45,7 @@ def download_audio(url, progress_hook=None):
             'preferredquality': '192',
         }],
         'force_generic_extractor': True,
+        'cookies':'--cookies-from-browser chrome',
         'ffmpeg_location': 'C:/Program Files/ffmpeg/bin',
         'ffprobe_location': 'C:/Program Files/ffmpeg/bin',
     }
@@ -80,6 +82,7 @@ def download_playlist(url, progress_hook=None):
         'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'),
         'format': 'best',
         'progress_hooks': [progress_hook] if progress_hook else [],
+        'cookies':'--cookies-from-browser chrome',
         'ffmpeg_location': 'C:/Program Files/ffmpeg/bin',
         'ffprobe_location': 'C:/Program Files/ffmpeg/bin',
     }
